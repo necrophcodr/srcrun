1ad8c557a062c233545f67e91846a1632d2950d1
necrophcodr <nc@scalehost.eu>
2023-08-02 15:03:52 +0200

    feat: add flake developer shell.

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, utils, ...}:
+  utils.lib.eachDefaultSystem(system:
+  let pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShell = pkgs.mkShell {
+      packages = with pkgs; [
+        tinycc
+        gnumake
+      ];
+    };
+  });
+}
